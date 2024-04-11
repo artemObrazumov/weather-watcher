00d7c6eb7fdc419b0f8df12f0a9c30b47f230fe4
artemObrazumov <a.obraz60022006@gmail.com>
2024-04-12 02:54:39 +0300

    common network di

---
+++ common/network/src/main/java/com/weatherwatcher/network/di/NetworkModule.kt
@@ -0,0 +1,20 @@
+package com.weatherwatcher.network.di
+
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(
+            HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+        ).build()
+}
